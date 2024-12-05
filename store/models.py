dfaa0a2372a7d27fd90adaf8e34ed48a46e45e8c
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-05 10:16:44 +0400

    init

---
+++ store/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    category = models.ManyToManyField(Category)
+    stock = models.PositiveIntegerField()
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    brand = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    thumbnail = models.URLField()
+
+
+    def __str__(self):
+        return self.title
