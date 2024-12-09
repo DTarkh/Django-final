b052825ffe12033c07009ffa214ece5bea33e761
David Tarkhnishvili <datotarkh@gmail.com>
2024-12-09 11:58:34 +0400

    Created endpoints for products_list and product_detail

--- store/views.py
+++ store/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+
+@api_view()
+def product_list(request):
+    query_set = Product.objects.all()
+    serialized_products = ProductSerializer(query_set, many=True)
+
+    return Response(serialized_products.data)
+@api_view()
+def product_detail(request,id):
+    product = get_object_or_404(Product, pk=id)
+    serialized_products = ProductSerializer(product)
+    return Response(serialized_products.data)
